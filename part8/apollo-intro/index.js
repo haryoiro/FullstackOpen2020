a65824089c9817bc135225b9e1bc2419fcebceb3
haryoiro <mizusecocolte@gmail.com>
2020-07-02 22:54:12 +0900

    [ADD] Part8 GraphQL example

---
+++ part8/apollo-intro/index.js
@@ -0,0 +1,79 @@
+const { ApolloServer, gql } = require('apollo-server')
+
+let persons = [
+  {
+    name: "Arto Hellas",
+    phone: "040-123543",
+    street: "Tapiolankatu 5 A",
+    city: "Espoo",
+    id: "3d594650-3436-11e9-bc57-8b80ba54c431"
+  },
+  {
+    name: "Matti Luukkainen",
+    phone: "040-432342",
+    street: "Malminkaari 10 A",
+    city: "Helsinki",
+    id: '3d599470-3436-11e9-bc57-8b80ba54c431'
+  },
+  {
+    name: "Venla Ruuska",
+    street: "Nallemäentie 22 C",
+    city: "Helsinki",
+    id: '3d599471-3436-11e9-bc57-8b80ba54c431'
+  },
+]
+
+/* Schema */
+/**
+ * データの構造を示す
+ * ID型はfield値の一意性を担保する
+ * エクスクラメーションマーク（！）はフィールドに対しnon-nullであることを強制する。
+ * (必須フィールド)
+ */
+const typeDefs = gql`
+  type Person {
+    name: String!
+    phone: String
+    street: String!
+    city: String!
+    id: ID!
+  }
+
+  type Query {
+    personCount: Int!
+    allPersons: [Person!]!
+    findPerson(name: String!): Person
+  }
+`
+
+/* Resolvers */
+// GraphQLクエリに対するレスポンスを定義
+
+const resolvers = {
+  Query: {
+    personCount: () => persons.length,
+    allPersons: () => persons,
+    findPerson: (root, args) => persons.find(p => p.name === args.name)
+  },
+  Person: {
+    name: (root) => root.name,
+    phone: (root) => root.phone,
+    street: (root) => root.street,
+    city: (root) => root.city,
+    id: (root) => root.id,
+  }
+}
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  playground: {
+    version: '1.7.25'
+  },
+})
+
+server
+  .listen()
+  .then(({ url }) => {
+    console.log(`🚀 SERVER READY AT ${url}`)
+  })
