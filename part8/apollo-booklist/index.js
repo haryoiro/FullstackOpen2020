1c166c0b0a78cd7722e3744dc101ed15a5a02217
haryoiro <mizusecocolte@gmail.com>
2020-07-05 15:26:49 +0900

    [UPDATE] 8.5: Books by genre

--- part8/apollo-booklist/index.js
+++ part8/apollo-booklist/index.js
@@ -87,7 +87,7 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks(author: String): [Books!]!
+    allBooks(author: String, genre: String): [Books!]!
     allAuthors: [Authors!]!
   }
   type Books {
@@ -109,9 +109,13 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: (root, args) => {
-      if (!args.author) return books
-      const byAuthor = books.filter(b => b.author === args.author)
-      return byAuthor
+      if (args.author && args.genre)
+        return books.filter(b => (b.author === args.author) && (b.genres.includes(args.genre)))
+      else if (args.genre)
+        return books.filter(b => b.genres.includes(args.genre))
+      else if (args.author)
+        return books.filter(b => b.author === args.author)
+      else books
     },
     allAuthors: (root, args) => authors
   },
