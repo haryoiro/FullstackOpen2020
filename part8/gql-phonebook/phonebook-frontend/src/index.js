9953edd6e56ad2993d2cf4e0a63e46eda323ccae
haryoiro <mizusecocolte@gmail.com>
2020-07-06 11:34:44 +0900

    [UPDATE] Apollo Client simplest example

--- part8/gql-phonebook/phonebook-frontend/src/index.js
+++ part8/gql-phonebook/phonebook-frontend/src/index.js
@@ -1,17 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import PromisePolyfill from 'promise-polyfill'
+import React from 'react'
+import { render } from 'react-dom'
+import App from './App'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+import {
+  ApolloClient,
+  ApolloProvider,
+  HttpLink,
+  InMemoryCache,
+  gql,
+} from '@apollo/client'
+
+if (!window.Promise) window.Promise = PromisePolyfill
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: 'http://localhost:4000',
+  })
+})
+
+const query = gql`
+query {
+  allPersons {
+    name,
+    phone,
+    address {
+      street,
+      city,
+    }
+    id
+  }
+}
+`
+
+client.query({ query })
+  .then((response) => {
+    console.log(response.data)
+  })
+
+render(
+  <App />,
   document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+)
