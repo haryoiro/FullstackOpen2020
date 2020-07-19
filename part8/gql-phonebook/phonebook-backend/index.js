254491e16e8d64d32741e47469b7a93bec2c9be3
haryoiro <mizusecocolte@gmail.com>
2020-07-19 15:59:52 +0900

    [UPDATE] connect to database

--- part8/gql-phonebook/phonebook-backend/index.js
+++ part8/gql-phonebook/phonebook-backend/index.js
@@ -1,29 +1,46 @@
+require('dotenv').config()
 const { ApolloServer, UserInputError, gql } = require('apollo-server')
-
+const mongoose = require('mongoose')
 const uuid = require('uuid/v1')
 
-let persons = [
-  {
-    name: "Arto Hellas",
-    phone: "040-123543",
-    street: "Tapiolankatu 5 A",
-    city: "Espoo",
-    id: "3d594650-3436-11e9-bc57-8b80ba54c431"
-  },
-  {
-    name: "Matti Luukkainen",
-    phone: "040-432342",
-    street: "Malminkaari 10 A",
-    city: "Helsinki",
-    id: '3d599470-3436-11e9-bc57-8b80ba54c431'
-  },
-  {
-    name: "Venla Ruuska",
-    street: "Nallemäentie 22 C",
-    city: "Helsinki",
-    id: '3d599471-3436-11e9-bc57-8b80ba54c431'
-  },
-]
+const { MONGODB_URI } = require('./util/config')
+const Person = require('./models/person.model')
+
+mongoose.set('useFindAndModify', false)
+console.log('--------------------------------')
+console.log('CONNECTING TO ', MONGODB_URI)
+console.log('--------------------------------')
+
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true })
+  .then(() => {
+    console.log('CONNECTED TO MONGODB')
+  })
+  .catch((error) => {
+    console.log('ERROR CONNECTION TO MONGODB: ', error.message)
+  })
+
+// let persons = [
+//   {
+//     name: "Arto Hellas",
+//     phone: "040-123543",
+//     street: "Tapiolankatu 5 A",
+//     city: "Espoo",
+//     id: "3d594650-3436-11e9-bc57-8b80ba54c431"
+//   },
+//   {
+//     name: "Matti Luukkainen",
+//     phone: "040-432342",
+//     street: "Malminkaari 10 A",
+//     city: "Helsinki",
+//     id: '3d599470-3436-11e9-bc57-8b80ba54c431'
+//   },
+//   {
+//     name: "Venla Ruuska",
+//     street: "Nallemäentie 22 C",
+//     city: "Helsinki",
+//     id: '3d599471-3436-11e9-bc57-8b80ba54c431'
+//   },
+// ]
 
 const typeDefs = gql`
   type Person {
@@ -55,56 +72,57 @@ const typeDefs = gql`
     editNumber(
       name: String!
       phone: String!
-    ): Person    
-  }  
+    ): Person
+  }
 `
 
 const resolvers = {
   Query: {
-    personCount: () => persons.length,
+    personCount: () => Person.collection.countDocuments(),
     allPersons: (root, args) => {
       if (!args.phone) {
-        return persons
+        return Person.find({})
       }
-
-      const byPhone = (person) =>
-        args.phone === 'YES' ? person.phone : !person.phone
-
-      return persons.filter(byPhone)
+      return Person.find({ phone: { $exists: args.phone === 'YES '}})
     },
     findPerson: (root, args) =>
-      persons.find(p => p.name === args.name)
+      Person.findOne({ name: args.name })
   },
   Person: {
     address: (root) => {
-      return { 
+      return {
         street: root.street,
-        city: root.city
+        city: root.city,
       }
     }
   },
   Mutation: {
-    addPerson: (root, args) => {
-      if (persons.find(p => p.name === args.name)) {
-        throw new UserInputError('Name must be unique', {
-          invalidArgs: args.name,
+    addPerson: async (root, args) => {
+      const person = new Person({ ...args })
+
+      try {
+        person.save()
+      } catch (error) {
+        throw new UserInputError(error.message, {
+          invalidArgs: args,
         })
       }
-      const person = { ...args, id: uuid() }
-      persons = persons.concat(person)
       return person
     },
-    editNumber: (root, args) => {
-      const person = persons.find(p => p.name === args.name)
-      if (!person) {
-        return null
+    editNumber: async (root, args) => {
+      const person = await Person.findOne({ name: args.name })
+      person.phone = args.phone
+
+      try {
+        person.save()
+      } catch (error) {
+        throw new UserInputError(erro.message, {
+          invalidArgs: args,
+        })
       }
-  
-      const updatedPerson = { ...person, phone: args.phone }
-      persons = persons.map(p => p.name === args.name ? updatedPerson : p)
-      return updatedPerson
-    }       
-  }  
+      return person
+    }
+  }
 }
 
 const server = new ApolloServer({
@@ -112,6 +130,8 @@ const server = new ApolloServer({
   resolvers,
 })
 
-server.listen().then(({ url }) => {
-  console.log(`Server ready at ${url}`)
-})
+server
+  .listen()
+  .then(({ url }) => {
+    console.log(`Server ready at ${url}`)
+  })
---
+++ part8/gql-phonebook/phonebook-backend/models/person.model.js
@@ -0,0 +1,26 @@
+const { Schema, model } = require('mongoose')
+
+const personSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: 5,
+  },
+  phone: {
+    type: String,
+    minlength: 5,
+  },
+  street: {
+    type: String,
+    required: true,
+    minlength: 5,
+  },
+  city: {
+    type: String,
+    required: true,
+    minlength: 3,
+  }
+})
+
+module.exports = model('Person', personSchema)
